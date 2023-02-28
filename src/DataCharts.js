8ccd3a012c19070c7574c17e5e0277876704d410
Nate <williamhamshar@gmail.com>
2023-02-28 01:38:28 -0500

    quick style fixes

--- src/DataCharts.js
+++ src/DataCharts.js
@@ -2,7 +2,7 @@ const DataCharts = () => {
 
     return (
         <div className="DataCharts">
-            <h3 className="dataChartsHeader">Data in the form of charts & graphs</h3>
+            <h3 className="dataChartsHeader">I am scared of making charts</h3>
         </div>
     )
 }
--- src/Intro.js
+++ src/Intro.js
@@ -3,7 +3,7 @@ import "./Intro.css";
 const Intro = () => {
     return (  
         <div className="introDiv">
-            <h1 className="introHeader">Loan Payment Calculator</h1>
+            <h1 className="introHeader">Loan Payment Pro</h1>
             <p className="introDescription">Looking to get a new loan for a car or a home? With this loan payment calculator, you can see just how much you will be expected to pay. The calculator also will provide you with some very helpful graphs that display how the balance will decrease over time. We just need to know a few things about your loan, so let's get started!</p>
         </div>
     );
